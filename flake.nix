ad8e2dcbc12b14d4bf1345a4a41833126ebda2d9
Max <max@privatevoid.net>
2023-03-26 15:32:22 +0200

    use nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "terranix flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     terranix-examples.url = "github:terranix/terranix-examples";
     bats-support = {
